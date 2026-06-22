ce6e9fbb33746574b34dceede56ba0cecef8419a
Dima <belyadima07@gmail.com>
2026-06-21 22:17:00 -0700

    added tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,12 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use std::net::TcpListener;
 
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-	App::new()
-	    .route("/health_check", web::get().to(health_check))
-	    //.route("/", web::get().to(greet))
-	    //.route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    // Bubble up the io::Error if we failed to binf the address
+    // Otherwise call .await on our Server
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
-
 
---
+++ tests/health_check.rs
@@ -0,0 +1,35 @@
+use std::{fmt::format, mem::zeroed, net::TcpListener};
+use zero2prod::run;
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+	.expect("Failed to execute a request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length()); 
+}
+
+// Launch our application in the background
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+
+    // Retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    // Return the application address to the caller
+    format!("http://127.0.0.1:{}", port)
+}
