e1f50c2d7c84679364e364705c6e660ed917b255
Dima <belyadima07@gmail.com>
2026-06-22 13:55:28 -0700

    passing 3 tests for health_check!

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,20 @@
-use std::{fmt::format, mem::zeroed, net::TcpListener};
-use zero2prod::run;
+use std::net::TcpListener;
+
+// Spin up an instance of our application
+// and retrurns irs address (i.e. http://localhost:XXXX)
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")  // 0 tells OS pick any
+        .expect("Failed to bind random port");
+
+    // Retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    // Return the application address to the caller
+    format!("http://127.0.0.1:{}", port)
+}
 
 #[tokio::test]
 async fn health_check_works() {
@@ -19,17 +34,54 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length()); 
 }
 
-// Launch our application in the background
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
 
-    // Retrieve the port assigned to us by the OS
-    let port = listener.local_addr().unwrap().port();
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+	.expect("Failed to execute request.");
+    
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
 
-    let server = zero2prod::run(listener).expect("Failed to bind address");
-    let _ = tokio::spawn(server);
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+	("name=le%20guin", "missing the email"),
+	("email=ursul_le_guin%40gmail.com", "missing the name"),
+	("", "missing both name and email")
+    ];
 
-    // Return the application address to the caller
-    format!("http://127.0.0.1:{}", port)
+    for (invalid_body, error_message) in test_cases {
+	// Act
+	let response = client
+	    .post(&format!("{}/subscriptions", &app_address))
+	    .header("Content-type", "application/x-www-form-urlencoded")
+	    .body(invalid_body)
+	    .send()
+	    .await
+	    .expect("Failed to execute request.");
+
+	// Assert
+	assert_eq!(
+	    400,
+	    response.status().as_u16(),
+	    // Additional customised error message on test failure
+	    "The API did not fail with 400 Bad Request when the payload was {}.",
+	    error_message
+	);
+    }
 }
