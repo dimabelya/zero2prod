1d9aab44d0f2f8bfeae78a87272c2bdf4eca35da
Dima <belyadima07@gmail.com>
2026-06-22 23:03:55 -0700

    creating a db table

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -23,6 +23,12 @@ APP_DB_NAME="${APP_DB_NAME:=newsletter}"
 # Allow to skip Docker if a dockerized Postgres db is alredy running
 if [[ -z "${SKIP_DOCKER}" ]]
 then
+  # if a postgres container is running, print instructions to kill it and exit
+  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+    docker rm -f "${RUNNING_POSTGRES_CONTAINER}"
+  fi
+
     # Launch postress using Docker, check if postgres is ready.
     CONTAINER_NAME="postgres"
     docker run \
