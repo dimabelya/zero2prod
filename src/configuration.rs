5626f97063e98cb0957b37355aa65df7d95e4b0a
Dima <belyadima07@gmail.com>
2026-06-25 21:19:50 -0700

    simplified readme, started to add config

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,14 +1,28 @@
-
-use actix_web::{App, HttpResponse, HttpServer, web};
-use actix_web::dev::Server;
-use std::net::TcpListener;
+use std::u16;
 
 #[derive(serde::Deserialize)]
-pub struct FormData {
-    email: String,
-    name: String
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16
 }
 
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+#[derive(serde::Deserialize, Clone)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // Initialise our configuration reader
+    let settings = config::Config::builder()
+        // Add configuration values from a file named `configuration.yaml`.
+        .add_source(
+            config::File::new("configuration.yaml", config::FileFormat::Yaml)
+        )
+        .build()?;
+    // Try to convert the configuration values it read into our Settings type
+    settings.try_deserialize::<Settings>()
 }
