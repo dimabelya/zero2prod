8609fd08d4ebdacbcbae670166fe65f9f091bec0
Dima <belyadima07@gmail.com>
2026-06-29 14:31:30 -0700

    queries are executed on the db! sqlx enforcing unique emailsgit add . next is test isolation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    // We have removed the hard-coded `8000` - it is now coming from our setings
+    let connection_pool = PgPool::connect(
+            &configuration.database.connection_string()
+        )
+        .await
+        .expect("Faield to connect to Postgres.");
+    
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,11 +1,40 @@
 use actix_web::{HttpResponse, web};
+use chrono::Utc;
+use uuid::Uuid;
+use sqlx::PgPool;
+use actix_web;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
-    _email: String,
-    _name: String,
+    email: String,
+    name: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+) -> HttpResponse {
+    // `Result` has two variants: `Ok` and `Err`.
+    // We use a `match` statement to choose what to do based on the outcome.
+        
+    match sqlx::query!(
+        r#"        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    // We use `get_ref` to get an immutable reference to
+    // the `PgConnection` wrapped by `web::Data`
+    .execute(pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
